1ff1f42e6bc3187cf9595885f38d46766ed19f04
nikita-kravtsov <payneblr@gmail.com>
2020-04-29 19:47:26 +0300

    init

---
+++ RestaurantApp/MainViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MainViewController.swift
+//  RestaurantApp
+//
+//  Created by Никита on 4/28/20.
+//  Copyright © 2020 Никита. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+                           "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+                           "Speak Easy", "Morris Pub", "Вкусные истории",
+                           "Классик", "Love&Life", "Шок", "Бочка"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+
+        
+        return cell
+    }
+    
+    //MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
