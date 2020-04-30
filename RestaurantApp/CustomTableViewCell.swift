c29b54d700827d5dddf41d2bb947a1f608594c32
nikita-kravtsov <payneblr@gmail.com>
2020-04-30 15:15:56 +0300

    Custom Table View Cell

---
+++ RestaurantApp/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  RestaurantApp
+//
+//  Created by Никита on 4/30/20.
+//  Copyright © 2020 Никита. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
